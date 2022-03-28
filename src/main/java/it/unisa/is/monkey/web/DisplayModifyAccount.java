901e10eb470e420b27d6ec27786792e4082197e0
GianmarcoDA <giggifrollino@gmail.com>
2022-03-28 18:07:05 +0200

    checkstyle
    Co-Authored-By: EmanueleZini <94375214+EmanueleZini@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayModifyAccount.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayModifyAccount.java
@@ -1,5 +1,7 @@
 package it.unisa.is.monkey.web;
 
+import it.unisa.is.monkey.applicationLogic.monkeyEntita.Utente;
+import it.unisa.is.monkey.model.MySqlUtenteDao;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,36 +10,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import it.unisa.is.monkey.applicationLogic.monkeyEntita.Utente;
-import it.unisa.is.monkey.model.MySqlUtenteDao;
-
-//Display modifica profilo lato utente
-
+ /**
+ * Display modifica profilo lato utente
+ */
 @WebServlet("/DisplayModifyAccount")
 public class DisplayModifyAccount extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    public DisplayModifyAccount() {
-        super();
+  public DisplayModifyAccount() {
+    super();
+  }
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+      synchronized(session) {
+      String id = request.getParameter("id");
+      MySqlUtenteDao utentedao = new MySqlUtenteDao();
+      Utente utente_x = utentedao.getUtente(id);
+      request.setAttribute("utenteX", utente_x);
+      request.getRequestDispatcher("modificaProfilo.jsp").forward(request, response);
     }
+  }
+  /**
+   * Mostra a schermo la modifica dell'account.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-            String id = request.getParameter("id");
-            MySqlUtenteDao utentedao = new MySqlUtenteDao();
-            Utente utente_x = utentedao.getUtente(id);
-            request.setAttribute("utenteX", utente_x);
-            request.getRequestDispatcher("modificaProfilo.jsp").forward(request, response);
-        }
-
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
-    }
-
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
 
 
