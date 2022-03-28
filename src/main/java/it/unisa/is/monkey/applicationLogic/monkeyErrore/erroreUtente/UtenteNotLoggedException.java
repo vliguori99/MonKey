49e8ed2d144779481664a0cdd14acf6ce8815854
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 19:59:15 +0200

    checkStyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/applicationLogic/monkeyErrore/erroreUtente/UtenteNotLoggedException.java
+++ src/main/java/it/unisa/is/monkey/applicationLogic/monkeyErrore/erroreUtente/UtenteNotLoggedException.java
@@ -1,13 +1,16 @@
 package it.unisa.is.monkey.applicationLogic.monkeyErrore.erroreUtente;
 
-public class UtenteNotLoggedException extends Exception{
+/**
+ * eccezione errore nel login
+ */
+public class UtenteNotLoggedException extends Exception {
+  private static String MESSAGGIO_DEFAULT = "errore nel login";
 
-    private static String MESSAGGIO_DEFAULT = "errore nel login";
-    public UtenteNotLoggedException(){
-        super(MESSAGGIO_DEFAULT);
-    }
+  public UtenteNotLoggedException() {
+    super(MESSAGGIO_DEFAULT);
+  }
 
-    public UtenteNotLoggedException(String messaggio){
-        super(messaggio);
-    }
+  public UtenteNotLoggedException(String messaggio) {
+    super(messaggio);
+  }
 }
