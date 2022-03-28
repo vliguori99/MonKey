2a95dbb25a66bf664cfb8ede3166be587481271d
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 17:28:26 +0200

    checkstyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayCart.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayCart.java
@@ -1,8 +1,9 @@
 package it.unisa.is.monkey.web;
 
+import it.unisa.is.monkey.applicationLogic.monkeyEntita.Prodotto;
+import it.unisa.is.monkey.model.MySqlProdottoDao;
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,47 +12,54 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-import it.unisa.is.monkey.applicationLogic.monkeyEntita.Prodotto;
-import it.unisa.is.monkey.model.*;
-
+/**
+ * La servlet mostra il display del carrello.
+ */
 @WebServlet("/DisplayCart")
 public class DisplayCart extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /*
-            @see HttpServlet#HttpServlet() salve
-    */
-    public DisplayCart() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * La classe mostra il display del carrello.
+   */
+  public DisplayCart() {
+    super();
+  }
+
+  /**
+   * La classe mostra il display del carrello.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      String utente = (String) session.getAttribute("userCode");
+      String ip = request.getRemoteAddr();
+      MySqlProdottoDao pdao = new MySqlProdottoDao();
+      List<Prodotto> cartList = pdao.getAllProductsIntoCart(utente, ip);
+      List<Integer> quantities = pdao.getQuantities(utente, ip);
+      request.setAttribute("list", cartList);
+      request.setAttribute("quantities", quantities);
+      RequestDispatcher rs = request.getRequestDispatcher("carrello.jsp");
+      rs.forward(request, response);
     }
+  }
 
-    /*
-           @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-    */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-            String utente = (String) session.getAttribute("userCode");
-            String ip = request.getRemoteAddr();
-            MySqlProdottoDao pdao = new MySqlProdottoDao();
-            List<Prodotto> cartList = pdao.getAllProductsIntoCart(utente, ip);
-            List<Integer> quantities = pdao.getQuantities(utente, ip);
-            request.setAttribute("list", cartList);
-            request.setAttribute("quantities", quantities);
-            RequestDispatcher rs = request.getRequestDispatcher("carrello.jsp");
-            rs.forward(request, response);
-        }
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+  /**
+   * La classe mostra il display del carrello.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
