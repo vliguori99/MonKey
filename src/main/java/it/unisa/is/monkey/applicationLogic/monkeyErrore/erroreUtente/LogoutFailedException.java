cbfda084efc1501de516b96d2299c3f49b7ea34e
vliguori99 <vliguori8@gmail.com>
2022-03-28 19:56:23 +0200

    CheckStyle Connection
    Co-Authored-By: EmanueleZini <94375214+EmanueleZini@users.noreply.github.com>
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/applicationLogic/monkeyErrore/erroreUtente/LogoutFailedException.java
+++ src/main/java/it/unisa/is/monkey/applicationLogic/monkeyErrore/erroreUtente/LogoutFailedException.java
@@ -1,13 +1,16 @@
 package it.unisa.is.monkey.applicationLogic.monkeyErrore.erroreUtente;
 
-public class LogoutFailedException extends Exception{
+/**
+ * Classe che gestisce l'eccezione del logout.
+ */
+public class LogoutFailedException extends Exception {
+  private static final String MESSAGGIO_DEFAULT = "errore nel logout";
 
-    private static String MESSAGGIO_DEFAULT = "errore nel logout";
-    public LogoutFailedException(){
-        super(MESSAGGIO_DEFAULT);
-    }
+  public LogoutFailedException() {
+    super(MESSAGGIO_DEFAULT);
+  }
 
-    public LogoutFailedException(String messaggio){
-        super(messaggio);
-    }
+  public LogoutFailedException(String messaggio) {
+    super(messaggio);
+  }
 }
--- src/main/java/utils/MySqlDao.java
+++ src/main/java/utils/MySqlDao.java
@@ -4,40 +4,39 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
+/**
+ * Classe che gestisce la connessione al DB.
+ */
 public class MySqlDao {
 
-    /** la classe driver */
-    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    /** Nome schema database */
-    public static final String SCHEMA = "Monkey";
-    /** Eventuali opzioni per il connettore JDBC */
-    public static final String OPTIONS =
-            "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC" ;
-    /** L'url al database */
-    public static final String DBURL = "jdbc:mysql://localhost:3306/" + SCHEMA + OPTIONS;
-    /** Lo username per le operazioni sul DB  */
-    public static final String USER = "root";
-    /** La password per le operazioni sul DB */
-    public static final String PASS = "root";
+  /** la classe driver. */
+  public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+  /** Nome schema database. */
+  public static final String SCHEMA = "Monkey";
+  /** Eventuali opzioni per il connettore JDBC. */
+  public static final String OPTIONS = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useL"
+          + "egacyDatetimeCode=false&server" + "Timezone=UTC";
+  /** L'url al database. */
+  public static final String DBURL = "jdbc:mysql://localhost:3306/" + SCHEMA + OPTIONS;
+  /** Lo username per le operazioni sul DB.  */
+  public static final String USER = "root";
+  /** La password per le operazioni sul DB. */
+  public static final String PASS = "root";
 
-    /**
-     * Metodo per creare una connessione sul DB MySQL
-     *
-     * @return l'oggetto Connection.
-     */
+  /**
+   * Metodo per creare una connessione sul DB MySQL.
+   *
+   * @return l'oggetto Connection.
+   */
 
-    public static Connection createConnection() {
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(DBURL, USER, PASS);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return conn;
+  public static Connection createConnection() {
+    Connection conn = null;
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      conn = DriverManager.getConnection(DBURL, USER, PASS);
+    } catch (SQLException | ClassNotFoundException e) {
+      e.printStackTrace();
     }
-
+    return conn;
+  }
 }
