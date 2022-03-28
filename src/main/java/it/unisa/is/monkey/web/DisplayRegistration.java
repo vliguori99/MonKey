363042f0c3bd785812c1d8b725901df8e698b48c
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 17:48:10 +0200

    checkstyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayRegistration.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayRegistration.java
@@ -1,9 +1,9 @@
 package it.unisa.is.monkey.web;
 
+import java.io.Console;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,40 +11,46 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import java.io.Console;
-
+/**
+ * La servlet mostra il display della registrazione.
+ */
 @WebServlet("/DisplayRegistration")
 public class DisplayRegistration extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplayRegistration() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * La classe mostra il display della registrazione.
+   */
+  public DisplayRegistration() {
+    super();
+  }
+
+  /**
+   * La classe mostra il display della registrazione.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      request.getRequestDispatcher("registrazione.jsp").forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-            request.getRequestDispatcher("registrazione.jsp").forward(request, response);
-        }
-
-    }
-
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+  /**
+   * La classe mostra il display della registrazione.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
