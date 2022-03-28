1b0bcc868dcc3e94969e8c48dd8a1f10994d5dec
GianmarcoDA <giggifrollino@gmail.com>
2022-03-28 17:44:59 +0200

    checkstyle
    Co-Authored-By: EmanueleZini <94375214+EmanueleZini@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayInfo.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayInfo.java
@@ -2,9 +2,7 @@ package it.unisa.is.monkey.web;
 
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Prodotto;
 import it.unisa.is.monkey.model.MySqlProdottoDao;
-
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,44 +11,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+/**
+ * Mostra a schermo le informazioni.
+ */
 @WebServlet("/DisplayInfo")
 public class DisplayInfo extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplayInfo() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+  /**
+   * La classw Mostra a schermo le informazioni.
+   */
+  public DisplayInfo() {
+    super();
+  }
+  /**
+   * Mostra a schermo le informazioni.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException,
             IOException {
-        HttpSession session = request.getSession();
+    HttpSession session = request.getSession();
 
-        synchronized (session) {
-            MySqlProdottoDao pdao = new MySqlProdottoDao();
-            String id = request.getParameter("id");
-            Prodotto prod = pdao.getProduct(id);
-            request.setAttribute("prodotto", prod);
-            RequestDispatcher rs = request.getRequestDispatcher("scheda.jsp");
-            rs.forward(request, response);
-        }
+    synchronized (session) {
+      MySqlProdottoDao pdao = new MySqlProdottoDao();
+      String id = request.getParameter("id");
+      Prodotto prod = pdao.getProduct(id);
+      request.setAttribute("prodotto", prod);
+      RequestDispatcher rs = request.getRequestDispatcher("scheda.jsp");
+      rs.forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+  /**
+   * Mostra a schermo le informazioni.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+    doGet(request, response);
+  }
 }
 
