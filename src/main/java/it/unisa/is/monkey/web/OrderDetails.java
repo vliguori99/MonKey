9fd901ed8e72efcc4739958f213e36a7320d1490
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 19:09:30 +0200

    Revert "Merge branch 'development' of https://github.com/vliguori99/Monkey into development"
    This reverts commit 0c9fd1a1a671965060b70a8346e07d83e3fa5692, reversing
    changes made to 8288cf2e844b69df4ce8525e4cb6d792551a4e56.

--- src/main/java/it/unisa/is/monkey/web/OrderDetails.java
+++ src/main/java/it/unisa/is/monkey/web/OrderDetails.java
@@ -4,9 +4,10 @@ import it.unisa.is.monkey.applicationLogic.monkeyEntita.Ordine;
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Prodotto;
 import it.unisa.is.monkey.model.MySqlOrdineDao;
 import it.unisa.is.monkey.model.MySqlProdottoDao;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,65 +16,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Mostra dettagli dell'ordine effettuato.
- */
 
 @WebServlet("/OrderDetails")
 public class OrderDetails extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  /**
-   * Classe che mostra i dettagli dell'ordine effettuato dall'utente.
-   */
-  public OrderDetails() {
-    super();
-  }
-  /**
-  * Mostra a schermo il login dell'account.
-  *
-  * @param request Richiede
-  * @param response Risponde
-  * @throws ServletException Eccezione servlet
-  * @throws IOException IO ecception
-  */
-
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    HttpSession session = request.getSession();
-
-    synchronized (session) {
-      MySqlProdottoDao pdao = new MySqlProdottoDao();
-      MySqlOrdineDao odao = new MySqlOrdineDao();
-      String codice = request.getParameter("codice");
-      Ordine ordine = odao.getOrder(codice);
-      List<Integer> quantita = odao.orderQuantities(codice);
-      List<String> codProdotti = odao.orderProducts(codice);
-      List<Prodotto> prodotti = new ArrayList<Prodotto>();
-      for (String s : codProdotti) {
-        prodotti.add(pdao.getProduct(s));
-      }
-      List<Float> prezzi = odao.orderPrices(codice);
-      request.setAttribute("order", ordine);
-      request.setAttribute("quantities", quantita);
-      request.setAttribute("prices", prezzi);
-      request.setAttribute("products", prodotti);
-      RequestDispatcher rs = request.getRequestDispatcher("dettagliOrdine.jsp");
-      rs.forward(request, response);
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OrderDetails() {
+        super();
+        // TODO Auto-generated constructor stub
     }
-  }
 
-  /**
-  * Mostra a schermo dettagli dell'ordine effettuato.
-  *
-  * @param request Richiede
-  * @param response Risponde
-  * @throws ServletException Eccezione servlet
-  * @throws IOException IO ecception
-  */
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
 
-    doGet(request, response);
-  }
+        synchronized(session) {
+            MySqlProdottoDao pdao = new MySqlProdottoDao();
+            MySqlOrdineDao odao = new MySqlOrdineDao();
+            String codice = request.getParameter("codice");
+            Ordine ordine = odao.getOrder(codice);
+            List<Integer> quantita = odao.orderQuantities(codice);
+            List<Float> prezzi = odao.orderPrices(codice);
+            List<String> codProdotti = odao.orderProducts(codice);
+            List<Prodotto> prodotti = new ArrayList<Prodotto>();
+            for(String s : codProdotti)
+            {
+                prodotti.add(pdao.getProduct(s));
+            }
+            request.setAttribute("order", ordine);
+            request.setAttribute("quantities", quantita);
+            request.setAttribute("prices", prezzi);
+            request.setAttribute("products", prodotti);
+            RequestDispatcher rs = request.getRequestDispatcher("dettagliOrdine.jsp");
+            rs.forward(request, response);
+        }
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
 }
