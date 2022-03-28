bb15cd207e068edad930181082300c02b78e41f2
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 17:14:18 +0200

    CheckStyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayAdminUsers.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayAdminUsers.java
@@ -2,10 +2,8 @@ package it.unisa.is.monkey.web;
 
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Utente;
 import it.unisa.is.monkey.model.MySqlUtenteDao;
-
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,43 +12,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-
-
-
+/**
+ *  La servlet mostra il display della gestione degli utenti(admin).
+ */
 @WebServlet("/DisplayAdminUsers")
 public class DisplayAdminUsers extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplayAdminUsers() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * La classe mostra il display della gestione degli utenti(admin).
+   */
+  public DisplayAdminUsers() {
+    super();
+  }
+
+  /**
+   * La classe mostra il display della gestione degli utenti(admin).
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      MySqlUtenteDao udao = new MySqlUtenteDao();
+      List<Utente> users = udao.getAllUtenti();
+      request.setAttribute("users", users);
+      RequestDispatcher rs = request.getRequestDispatcher("adminUtenti.jsp");
+      rs.forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-            MySqlUtenteDao udao = new MySqlUtenteDao();
-            List<Utente> users = udao.getAllUtenti();
-            request.setAttribute("users", users);
-            RequestDispatcher rs = request.getRequestDispatcher("adminUtenti.jsp");
-            rs.forward(request, response);
-        }
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+  /**
+   * La classe mostra il display della gestione degli utenti(admin).
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
