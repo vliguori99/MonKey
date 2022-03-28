5639fdcfbd72ad5ed5600a01f73d0c15664d9a1b
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 17:43:13 +0200

    checkstyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayLogin.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayLogin.java
@@ -1,9 +1,9 @@
 package it.unisa.is.monkey.web;
 
+import java.io.Console;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,52 +12,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import java.io.Console;
-
+/**
+ * La servlet che mostra il display del login.
+ */
 @WebServlet("/DisplayLogin")
 public class DisplayLogin extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplayLogin() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * La classe che mostra il display del login.
+   */
+  public DisplayLogin() {
+    super();
+  }
+
+  /**
+   * La classe che mostra il display del login.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      String userCode = null;
+      userCode = (String) session.getAttribute("userCode");
+      RequestDispatcher rs = null;
+      request.setAttribute("loginError", false);
+      if (userCode == null) {
+        rs = request.getRequestDispatcher("login.jsp");
+      } else {
+        rs = request.getRequestDispatcher("DisplayInfoUtente");
+      }
+      rs.forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-            String userCode = null;
-            userCode = (String) session.getAttribute("userCode");
-            RequestDispatcher rs = null;
-            request.setAttribute("loginError", false);
-            if (userCode == null)
-            {
-                rs = request.getRequestDispatcher("login.jsp");
-            }
-            else
-            {
-                rs = request.getRequestDispatcher("DisplayInfoUtente");
-            }
-            rs.forward(request, response);
-        }
-
-    }
-
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+  /**
+   * La classe che mostra il display del login.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
