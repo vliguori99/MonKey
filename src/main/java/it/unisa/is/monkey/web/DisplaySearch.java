ba1c4188431a06860f972f438346ff1fbd26fc34
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 17:52:42 +0200

    checkstyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplaySearch.java
+++ src/main/java/it/unisa/is/monkey/web/DisplaySearch.java
@@ -2,10 +2,8 @@ package it.unisa.is.monkey.web;
 
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Prodotto;
 import it.unisa.is.monkey.model.MySqlProdottoDao;
-
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,41 +12,52 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-
+/**
+ * La servlet mostra il display di ricerca.
+ */
 
 @WebServlet("/DisplaySearch")
 public class DisplaySearch extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplaySearch() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+ /**
+  * La classe mostra il display di ricerca.
+  */
+  public DisplaySearch() {
+    super();
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
+  /**
+   * La classe mostra il display di ricerca.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      String search = request.getParameter("s");
+      MySqlProdottoDao pdao = new MySqlProdottoDao();
+      List<Prodotto> searchList = pdao.getSearchProducts(search);
+      request.setAttribute("searchList", searchList);
+      RequestDispatcher rs = request.getRequestDispatcher("ricerca.jsp");
+      rs.forward(request, response);
+    }
+  }
 
-        synchronized(session) {
-            String search = request.getParameter("s");
-            MySqlProdottoDao pdao = new MySqlProdottoDao();
-            List<Prodotto> searchList = pdao.getSearchProducts(search);
-            request.setAttribute("searchList", searchList);
-            RequestDispatcher rs = request.getRequestDispatcher("ricerca.jsp");
-            rs.forward(request, response);
-        }
-    }
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
+  /**
+   * La classe mostra il display di ricerca.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
