0430fc9a629c4601f42e31b146f73cdfa436aac1
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 17:59:08 +0200

    checkstyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayUpdateForm.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayUpdateForm.java
@@ -2,9 +2,7 @@ package it.unisa.is.monkey.web;
 
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Prodotto;
 import it.unisa.is.monkey.model.MySqlProdottoDao;
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,56 +10,65 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * La servlet mostra il display del form per la modifica prodotto.
+ */
 @WebServlet("/DisplayUpdateForm")
 public class DisplayUpdateForm extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplayUpdateForm() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * La classe mostra il display del form per la modifica prodotto.
+   */
+  public DisplayUpdateForm() {
+    super();
+  }
+
+  /**
+   * La classe mostra il display del form per la modifica prodotto.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      String codice = request.getParameter("codice");
+      MySqlProdottoDao pdao = new MySqlProdottoDao();
+      Prodotto p = pdao.getProduct(codice);
+      String titolo = p.getTitolo();
+      Float prezzo_listino = p.getPrezzo_listino();
+      Float sconto = p.getSconto_attuale();
+      String piattaforma = p.getPiattaforma();
+      String tipologia = p.getTipologia();
+      String descrizione = p.getDescrizione();
+      int quantita = p.getQuantita();
+      request.setAttribute("codice", codice);
+      request.setAttribute("titolo", titolo);
+      request.setAttribute("prezzo_listino", prezzo_listino);
+      request.setAttribute("sconto", sconto);
+      request.setAttribute("piattaforma", piattaforma);
+      request.setAttribute("tipologia", tipologia);
+      request.setAttribute("descrizione", descrizione);
+      request.setAttribute("quantita", quantita);
+      request.getRequestDispatcher("modificaProdotto.jsp").forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-            String codice = request.getParameter("codice");
-            MySqlProdottoDao pdao = new MySqlProdottoDao();
-            Prodotto p = pdao.getProduct(codice);
-            String titolo = p.getTitolo();
-            Float prezzo_listino = p.getPrezzo_listino();
-            Float sconto = p.getSconto_attuale();
-            String piattaforma = p.getPiattaforma();
-            String tipologia = p.getTipologia();
-            String descrizione = p.getDescrizione();
-            int quantita = p.getQuantita();
-            request.setAttribute("codice", codice);
-            request.setAttribute("titolo", titolo);
-            request.setAttribute("prezzo_listino", prezzo_listino);
-            request.setAttribute("sconto", sconto);
-            request.setAttribute("piattaforma", piattaforma);
-            request.setAttribute("tipologia", tipologia);
-            request.setAttribute("descrizione", descrizione);
-            request.setAttribute("quantita", quantita);
-            request.getRequestDispatcher("modificaProdotto.jsp").forward(request, response);
-        }
-
-    }
-
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
+  /**
+   * La classe mostra il display del form per la modifica prodotto.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 
 }
