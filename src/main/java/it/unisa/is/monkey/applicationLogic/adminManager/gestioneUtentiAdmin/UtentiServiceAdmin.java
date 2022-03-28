04a07a4edc3314839043d76bc9cdf9b6c4947765
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 19:54:29 +0200

    checkstyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/applicationLogic/adminManager/gestioneUtentiAdmin/UtentiServiceAdmin.java
+++ src/main/java/it/unisa/is/monkey/applicationLogic/adminManager/gestioneUtentiAdmin/UtentiServiceAdmin.java
@@ -4,31 +4,35 @@ import it.unisa.is.monkey.applicationLogic.monkeyEntita.Utente;
 import it.unisa.is.monkey.applicationLogic.monkeyErrore.erroreUtente.UserNotDeletedException;
 import it.unisa.is.monkey.model.MySqlUtenteDao;
 
-public class UtentiServiceAdmin implements UtentiServiceAdminInterface{
+/**
+ * Classe che gestisce servizi admin.
+ */
+public class UtentiServiceAdmin implements UtentiServiceAdminInterface {
+  private MySqlUtenteDao utenteDAO = new MySqlUtenteDao();
 
-    private MySqlUtenteDao utenteDAO = new MySqlUtenteDao();
-
-    @Override
-    public void rimozioneUtente(String utente) throws UserNotDeletedException {
-        if (utente == null){
-            throw new UserNotDeletedException("Utente non valido");
-        }
-        utenteDAO.deleteUtente(utente);
+  /**
+   * metodo che rimuove l'utente dal db.
+   *
+   * @param utente identifica l'utente
+   * @throws UserNotDeletedException eccezione nel caso in cui utente non valido
+   */
+  @Override
+  public void rimozioneUtente(String utente) throws UserNotDeletedException {
+    if (utente == null) {
+      throw new UserNotDeletedException("Utente non valido");
     }
+    utenteDAO.deleteUtente(utente);
+  }
 
-    @Override
-    public Utente modificaUtenteAdmin(String idUtente, boolean admin) {
-
-        Utente daModificare = utenteDAO.getUtente(idUtente);
-
-        if(daModificare.getAmministratore() != admin){
-            daModificare.setAmministratore(admin);
-            utenteDAO.updateUtente(daModificare);
-        }
-        return daModificare;
+  @Override
+  public Utente modificaUtenteAdmin(String idUtente, boolean admin) {
+    Utente daModificare = utenteDAO.getUtente(idUtente);
+    if (daModificare.getAmministratore() != admin) {
+      daModificare.setAmministratore(admin);
+      utenteDAO.updateUtente(daModificare);
     }
-
-
+    return daModificare;
+  }
 }
 
 
