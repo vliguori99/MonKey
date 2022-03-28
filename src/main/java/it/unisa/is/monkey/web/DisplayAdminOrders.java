25226d962318aab51887d81ddb67db4520330fe9
GianmarcoDA <giggifrollino@gmail.com>
2022-03-28 17:28:20 +0200

    checkstyle
    Co-Authored-By: EmanueleZini <94375214+EmanueleZini@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayAdminOrders.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayAdminOrders.java
@@ -3,10 +3,9 @@ package it.unisa.is.monkey.web;
 import it.unisa.is.monkey.applicationLogic.adminManager.gestioneOrdineAdmin.OrdiniServiceAdmin;
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Ordine;
 import it.unisa.is.monkey.applicationLogic.monkeyErrore.erroreUtente.UtenteNotLoggedException;
-
 import java.io.IOException;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,53 +14,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+/**
+ * mostra il display degli ordini da lato Admin.
+ */
 @WebServlet("/DisplayAdminOrders")
 public class DisplayAdminOrders extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplayAdminOrders() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * la classe mostra il display degli ordini da lato Admin.
+   */
+  public DisplayAdminOrders() {
+    super();
+  }
+
+  /**
+   * mostra il display degli ordini da lato Admin.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      String data1 = request.getParameter("data1");
+      String data2 = request.getParameter("data2");
+      OrdiniServiceAdmin ordiniAdmin = new OrdiniServiceAdmin();
+      String idUtente = (String) request.getParameter("utente");
+      List<Ordine> ordini = new ArrayList<>();
+      try {
+        ordini = ordiniAdmin.visualizzaOrdini(data1, data2, idUtente);
+      } catch (UtenteNotLoggedException e) {
+        e.printStackTrace();
+      }
+      request.setAttribute("ordini", ordini);
+      RequestDispatcher rs = request.getRequestDispatcher("adminOrdini.jsp");
+      rs.forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-
-            String data1 = request.getParameter("data1");
-            String data2 = request.getParameter("data2");
-
-            OrdiniServiceAdmin ordiniAdmin = new OrdiniServiceAdmin();
-            String idUtente = (String) request.getParameter("utente");
-
-            List<Ordine> ordini = new ArrayList<>();
-
-            try {
-                ordini = ordiniAdmin.visualizzaOrdini(data1, data2, idUtente);
-            } catch (UtenteNotLoggedException e) {
-                e.printStackTrace();
-            }
-
-            request.setAttribute("ordini", ordini);
-            RequestDispatcher rs = request.getRequestDispatcher("adminOrdini.jsp");
-            rs.forward(request, response);
-        }
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+  /**
+   * mostra il display degli ordini da lato Admin.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
