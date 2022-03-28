1fe883cbf3e25802a7a55fcdea5765e5694b33a9
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 21:44:05 +0200

    Check style
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/OrderDetails.java
+++ src/main/java/it/unisa/is/monkey/web/OrderDetails.java
@@ -4,10 +4,9 @@ import it.unisa.is.monkey.applicationLogic.monkeyEntita.Ordine;
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Prodotto;
 import it.unisa.is.monkey.model.MySqlOrdineDao;
 import it.unisa.is.monkey.model.MySqlProdottoDao;
-
 import java.io.IOException;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,53 +15,62 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+/**
+ * La servlet identifica i dettagli degli ordini.
+ */
 @WebServlet("/OrderDetails")
 public class OrderDetails extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public OrderDetails() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * La classe identifica i dettagli degli ordini.
+   */
+  public OrderDetails() {
+    super();
+  }
+
+  /**
+   * La classe identifica i dettagli degli ordini.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      MySqlProdottoDao pdao = new MySqlProdottoDao();
+      MySqlOrdineDao odao = new MySqlOrdineDao();
+      String codice = request.getParameter("codice");
+      Ordine ordine = odao.getOrder(codice);
+      List<Integer> quantita = odao.orderQuantities(codice);
+      List<String> codProdotti = odao.orderProducts(codice);
+      List<Prodotto> prodotti = new ArrayList<Prodotto>();
+      for (String s : codProdotti) {
+        prodotti.add(pdao.getProduct(s));
+      }
+      List<Float> prezzi = odao.orderPrices(codice);
+      request.setAttribute("order", ordine);
+      request.setAttribute("quantities", quantita);
+      request.setAttribute("prices", prezzi);
+      request.setAttribute("products", prodotti);
+      RequestDispatcher rs = request.getRequestDispatcher("dettagliOrdine.jsp");
+      rs.forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-            MySqlProdottoDao pdao = new MySqlProdottoDao();
-            MySqlOrdineDao odao = new MySqlOrdineDao();
-            String codice = request.getParameter("codice");
-            Ordine ordine = odao.getOrder(codice);
-            List<Integer> quantita = odao.orderQuantities(codice);
-            List<Float> prezzi = odao.orderPrices(codice);
-            List<String> codProdotti = odao.orderProducts(codice);
-            List<Prodotto> prodotti = new ArrayList<Prodotto>();
-            for(String s : codProdotti)
-            {
-                prodotti.add(pdao.getProduct(s));
-            }
-            request.setAttribute("order", ordine);
-            request.setAttribute("quantities", quantita);
-            request.setAttribute("prices", prezzi);
-            request.setAttribute("products", prodotti);
-            RequestDispatcher rs = request.getRequestDispatcher("dettagliOrdine.jsp");
-            rs.forward(request, response);
-        }
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+  /**
+   * La classe identifica i dettagli degli ordini.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
