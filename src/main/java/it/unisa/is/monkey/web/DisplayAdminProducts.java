bc74aebb7e1f8f4630f83f52778b3ee96865d73b
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 17:09:31 +0200

    checkstyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/DisplayAdminProducts.java
+++ src/main/java/it/unisa/is/monkey/web/DisplayAdminProducts.java
@@ -2,10 +2,8 @@ package it.unisa.is.monkey.web;
 
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Prodotto;
 import it.unisa.is.monkey.model.MySqlProdottoDao;
-
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,40 +12,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+/**
+ * La servlet mostra il display della gestione prodotti(admin).
+ */
 @WebServlet("/DisplayAdminProducts")
 public class DisplayAdminProducts extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplayAdminProducts() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * La classe mostra il display della gestione prodotti(admin).
+   */
+  public DisplayAdminProducts() {
+    super();
+  }
+
+  /**
+   * La classe mostra il display della gestione prodotti(admin).
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      MySqlProdottoDao pdao = new MySqlProdottoDao();
+      List<Prodotto> products = pdao.getAllProducts();
+      request.setAttribute("products", products);
+      RequestDispatcher rs = request.getRequestDispatcher("adminProdotti.jsp");
+      rs.forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-
-        synchronized(session) {
-            MySqlProdottoDao pdao = new MySqlProdottoDao();
-            List<Prodotto> products = pdao.getAllProducts();
-            request.setAttribute("products", products);
-            RequestDispatcher rs = request.getRequestDispatcher("adminProdotti.jsp");
-            rs.forward(request, response);
-        }
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+  /**
+   * La classe mostra il display della gestione prodotti(admin).
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
