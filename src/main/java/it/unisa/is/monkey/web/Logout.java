c6952a6d237bd146d0a6f0a7418002cc03cfe824
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 18:12:29 +0200

    checkstyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/web/Logout.java
+++ src/main/java/it/unisa/is/monkey/web/Logout.java
@@ -1,7 +1,6 @@
 package it.unisa.is.monkey.web;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,42 +9,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+/**
+ * La servlet serve a fare il logout.
+ */
 @WebServlet("/Logout")
 public class Logout extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Logout() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * La classe serve a fare il logout.
+   */
+  public Logout() {
+    super();
+  }
+
+  /**
+   * La classe serve a fare il logout.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    RequestDispatcher rs = null;
+    synchronized (session) {
+      session.setAttribute("userCode", null);
+      session.setAttribute("isAdmin", null);
+      rs = request.getRequestDispatcher("index.jsp");
+      rs.forward(request, response);
+      return;
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        HttpSession session = request.getSession();
-        RequestDispatcher rs = null;
-        synchronized(session) {
-            session.setAttribute("userCode", null);
-            session.setAttribute("isAdmin", null);
-            rs = request.getRequestDispatcher("index.jsp");
-            rs.forward(request, response);
-            return;
-        }
-    }
-
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
+  /**
+   * La classe serve a fare il logout.
+   *
+   * @param request Richiede
+   * @param response Risponde
+   * @throws ServletException Eccezione servlet
+   * @throws IOException IO ecception
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
