5661f10746acdcc0eced69106cc999a5ef4b112c
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 21:46:27 +0200

    Check style
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/applicationLogic/monkeyErrore/erroreProdotto/OrderNotFoundException.java
+++ src/main/java/it/unisa/is/monkey/applicationLogic/monkeyErrore/erroreProdotto/OrderNotFoundException.java
@@ -1,13 +1,18 @@
 package it.unisa.is.monkey.applicationLogic.monkeyErrore.erroreProdotto;
 
-public class OrderNotFoundException extends Exception{
+/**
+ * Eccezione nell'errore nella visualizzazione degli ordini.
+ */
+public class OrderNotFoundException extends Exception {
 
-    private static String MESSAGGIO_DEFAULT = "errore nella visualizzazione dell'ordine";
-    public OrderNotFoundException(){
-        super(MESSAGGIO_DEFAULT);
-    }
+  private static String MESSAGGIO_DEFAULT = "errore nella visualizzazione dell'ordine";
 
-    public OrderNotFoundException(String messaggio){
-        super(messaggio);
-    }
+  public OrderNotFoundException() {
+    super(MESSAGGIO_DEFAULT);
+  }
+
+  public OrderNotFoundException(String messaggio) {
+    super(messaggio);
+  }
+  
 }
