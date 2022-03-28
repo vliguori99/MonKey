444e0582befd0386fb1bd8d971ad57080fefac44
EmanueleZini <emanuelezn@gmail.com>
2022-03-28 23:12:01 +0200

    CheckStyle
    Co-Authored-By: GianmarcoDA <97538754+GianmarcoDA@users.noreply.github.com>
    Co-Authored-By: vliguori99 <97514620+vliguori99@users.noreply.github.com>

--- src/main/java/it/unisa/is/monkey/applicationLogic/monkeyErrore/erroreUtente/UserNotDeletedException.java
+++ src/main/java/it/unisa/is/monkey/applicationLogic/monkeyErrore/erroreUtente/UserNotDeletedException.java
@@ -1,13 +1,17 @@
 package it.unisa.is.monkey.applicationLogic.monkeyErrore.erroreUtente;
 
+/**
+ * Eccezione errore nell'eliminazione.
+ */
 public class UserNotDeletedException extends Exception {
 
-    private static String MESSAGGIO_DEFAULT = "errore nell'eliminazione";
-    public UserNotDeletedException(){
-        super(MESSAGGIO_DEFAULT);
-    }
+  private static String MESSAGGIO_DEFAULT = "errore nell'eliminazione";
 
-    public UserNotDeletedException(String messaggio) {
-        super(messaggio);
-    }
+  public UserNotDeletedException() {
+    super(MESSAGGIO_DEFAULT);
+  }
+
+  public UserNotDeletedException(String messaggio) {
+    super(messaggio);
+  }
 }
--- src/main/java/it/unisa/is/monkey/applicationLogic/userManager/gestioneRegistrazione/RegistrazioneServiceInterface.java
+++ src/main/java/it/unisa/is/monkey/applicationLogic/userManager/gestioneRegistrazione/RegistrazioneServiceInterface.java
@@ -3,8 +3,12 @@ package it.unisa.is.monkey.applicationLogic.userManager.gestioneRegistrazione;
 import it.unisa.is.monkey.applicationLogic.monkeyEntita.Utente;
 import it.unisa.is.monkey.applicationLogic.monkeyErrore.erroreUtente.UserNotRegisteredException;
 
+/**
+ * Interfaccia registrazione.
+ */
 public interface RegistrazioneServiceInterface {
 
-    Utente registrazione(String nome, String cognome, String username, String email, String psw,
-                         String indirizzo, String numCarta, boolean amministratore) throws UserNotRegisteredException;
+  Utente registrazione(String nome, String cognome, String username, String email, String psw,
+                         String indirizzo, String numCarta, boolean amministratore)
+          throws UserNotRegisteredException;
 }
